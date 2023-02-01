801549ea2d519845a27791ed6e29f17411438499
Joseph Martin <josephmartin1350@gmail.com>
2023-02-01 12:49:46 -0700

    commit tree ec2ae6dff27b37f9b3a6c78add6766e3cfb1255d

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,14 @@
 class Author < ApplicationRecord
+    has_many :posts
+
+    has_one :profile
+end
+
+class Profile < ApplicationRecord
+    belongs_to :author
+    belongs_to :tag
+end
+
+class Tag < ApplicationRecord
+    has_many :post_tags
 end
